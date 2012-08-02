0f1805c40e98140688d2cd44563b0caf785743b2
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2012-08-03 03:29:40 +0530

    Adding principal components tab

--- source/CsvModel/CsvModel.py
+++ source/CsvModel/CsvModel.py
@@ -5,18 +5,26 @@ import csv
 from traits.api import File, HasTraits, Array, List, Instance
 from enaml.stdlib.table_model import TableModel
 from chaco.api import Plot, ArrayPlotData
+from sklearn.decomposition import PCA
 
 class CsvModel(HasTraits):
     
     filename = File
     table = Array
     headers = List
+    pca = Instance(PCA,())
     xvsy_indices = (0,1)
     table_model = Instance(TableModel,())
     img_plotdata = Instance(ArrayPlotData,())
     image_plot = Instance(Plot,())
     xvsy_plotdata = Instance(ArrayPlotData, ())
     x_vs_y_plot = Instance(Plot, ())
+    pca_plotdata = Instance(ArrayPlotData,())
+    pca_plot = Instance(Plot,())
+    
+    def __init__(self):
+        self.pca = PCA(n_components=2)
+        self.pca.whiten = True
     
     def _table_default(self):
         x = np.zeros((100,100))
@@ -39,6 +47,13 @@ class CsvModel(HasTraits):
         p.plot(("x","y"), type='scatter',color='auto')
         return p
     
+    def _pca_plot_default(self):
+        pc_red = self.pca.fit_transform(self.table[0:100,:])
+        self.pca_plotdata = ArrayPlotData(x=pc_red[:,0],y=pc_red[:,1])
+        pca_plot = Plot(self.pca_plotdata)
+        pca_plot.plot(("x","y"),type='scatter',color='auto')
+        return pca_plot
+    
     def _filename_changed(self, new):
         self.table = np.genfromtxt(self.filename, delimiter=',', skip_header=1)
         csv_reader= csv.reader(file(self.filename))
@@ -47,4 +62,5 @@ class CsvModel(HasTraits):
         self.table_model = TableModel(self.table, editable=True,
                                  horizontal_headers=self.headers)
         self.img_plotdata.set_data('imagedata',self.table)
+    
     
