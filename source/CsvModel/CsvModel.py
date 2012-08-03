2be332192ebd4ccafd9bff4bd0f5c329b968250b
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2012-08-03 07:09:02 +0530

    Added some more core functions

--- source/CsvModel/CsvModel.py
+++ source/CsvModel/CsvModel.py
@@ -2,18 +2,26 @@
 
 import numpy as np
 import csv
-from traits.api import File, HasTraits, Array, List, Instance
+from traits.api import (
+    File, HasTraits, Array, List, Instance, Function, Int, Float
+)
 from enaml.stdlib.table_model import TableModel
 from chaco.api import Plot, ArrayPlotData
 from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
 
 class CsvModel(HasTraits):
     
     filename = File
     table = Array
     headers = List
-    pca = Instance(PCA,())
+    pca = Instance(PCA)
     xvsy_indices = (0,1)
+    hist_row_index = Int(2)
+    hist_nbins = Int(10)
+    block_mean = Float
+    block_var = Float
+    block_std = Float
     table_model = Instance(TableModel,())
     img_plotdata = Instance(ArrayPlotData,())
     image_plot = Instance(Plot,())
@@ -21,6 +29,8 @@ class CsvModel(HasTraits):
     x_vs_y_plot = Instance(Plot, ())
     pca_plotdata = Instance(ArrayPlotData,())
     pca_plot = Instance(Plot,())
+    hist_plotdata = Instance(ArrayPlotData,())
+    hist_plot = Instance(Plot,())
     
     def __init__(self):
         self.pca = PCA(n_components=2)
@@ -54,6 +64,15 @@ class CsvModel(HasTraits):
         pca_plot.plot(("x","y"),type='scatter',color='auto')
         return pca_plot
     
+    def _hist_plot_default(self):
+        h = np.histogram(self.table,10)[0]
+        self.hist_plotdata = ArrayPlotData(x=h)
+        p = Plot(self.hist_plotdata)
+        p.plot('x',type='bar',color='auto',bar_width=0.3)
+        return p
+    
+    
+
     def _filename_changed(self, new):
         self.table = np.genfromtxt(self.filename, delimiter=',', skip_header=1)
         csv_reader= csv.reader(file(self.filename))
@@ -62,5 +81,10 @@ class CsvModel(HasTraits):
         self.table_model = TableModel(self.table, editable=True,
                                  horizontal_headers=self.headers)
         self.img_plotdata.set_data('imagedata',self.table)
+        self.xvsy_plotdata.set_data('x',self.table[:,self.xvsy_indices[0]])
+        self.xvsy_plotdata.set_data('y',self.table[:,self.xvsy_indices[1]])
+        self.hist_plotdata.set_data(
+            'x',np.histogram(self.table[:,self.hist_row_index],self.hist_nbins)[0]
+            )
     
     
