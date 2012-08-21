fba915a73d169034cb25abd275130435e0430e2c
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2012-08-22 05:02:27 +0530

    Adding a python class to handle current selections in TableViews

--- source/scripting_widgets/selection_handler.py
+++ source/scripting_widgets/selection_handler.py
@@ -1,12 +1,28 @@
-from traits.api import List, HasTraits
+# Classes for converting ModelIndices into
+
+from traits.api import HasTraits, List
+
 
 class SelectionHandler(HasTraits):
     
-    selection = List
     current_selection = List
-    
-    def _selection_default(self):
-        return []
+    selected_indices = List
     
     def _current_selection_default(self):
         return []
+    
+    def _selected_indices_default(self):
+        return []
+    
+    def create_selection(self):
+        for elem in self.current_selection:
+            self.selected_indices.append(
+                (elem[0].row,
+                 elem[0].column,
+                 elem[1].row,
+                 elem[1].column)
+            )
+    
+    def flush(self):
+        self.curren_selection = []
+        self.selected_indices = []
