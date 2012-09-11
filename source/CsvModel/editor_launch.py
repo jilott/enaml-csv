b49b87a15892d866cd433431e0ed6a1c537b800b
Jaidev Deshpande <deshpande.jaidev@gmail.com>
2012-09-11 14:06:59 +0530

    Added MATLAB-like command history

--- source/CsvModel/editor_launch.py
+++ source/CsvModel/editor_launch.py
@@ -2,6 +2,8 @@ from CsvModel import CsvModel
 from plot_handlers import (XYPlotHandler, ImagePlotHandler, PCPlotHandler,
 RegressionPlotHandler, HistogramPlotHandler, KMeansPlotHandler)
 from text_tools import TextClassifier
+import os
+import pickle
 
 model = CsvModel()
 xyplot_handler = XYPlotHandler()
@@ -26,6 +28,18 @@ def enaml_editor_launch(x, AS_PANDAS=False):
     histplot_handler = HistogramPlotHandler()
     kmeans_plot = KMeansPlotHandler()
     
+    this_path = os.path.abspath(__file__)
+    model_dir = os.path.dirname(this_path)
+    command_history_file = os.path.join(model_dir, 'command_history.pkl')
+    
+    if os.path.isfile(command_history_file):
+        f = open(command_history_file, 'r')
+        command_history = pickle.load(f)
+        model.script_handler.command_history = command_history
+        f.close()
+    
+    
+    
     import enaml
     with enaml.imports():
         from layout import Main
@@ -40,6 +54,18 @@ def enaml_editor_launch(x, AS_PANDAS=False):
             text_class = text_class
         )
         view.show()
+
+
+this_path = os.path.abspath(__file__)
+model_dir = os.path.dirname(this_path)
+command_history_file = os.path.join(model_dir, 'command_history.pkl')
+
+if os.path.isfile(command_history_file):
+    f = open(command_history_file, 'r')
+    command_history = pickle.load(f)
+    model.script_handler.command_history = command_history
+    f.close()
+
 
 if __name__ == '__main__':
     import enaml
--- source/CsvModel/script_handler.py
+++ source/CsvModel/script_handler.py
@@ -1,4 +1,6 @@
-from traits.api import HasTraits, String, Dict
+from traits.api import HasTraits, String, Dict, List
+import pickle
+import os
 
 class ScriptHandler(HasTraits):
     '''
@@ -15,6 +17,9 @@ class ScriptHandler(HasTraits):
     
     # Local variables after 'exec'uting the script
     my_globals = Dict
+    
+    # The list of commands executed to date
+    command_history = List
     
     def _script_default(self):
         return ''
@@ -35,3 +40,35 @@ class ScriptHandler(HasTraits):
     def _my_locals_items_changed(self, new):
         # Implement this to change items in the workspace view
         pass
+    
+    def _command_history_default(self):
+        return []
+    
+    def exec_single_line(self, clicked_event):
+        command_index = clicked_event.new.row
+        exec(self.command_history[command_index]) in self.my_globals, \
+            self.my_locals
+        self.command_history.append(self.command_history[command_index])
+    
+    def save_workspace(self):
+        
+        this_path = os.path.abspath(__file__)
+        model_dir = os.path.dirname(this_path)
+        command_history_file = os.path.join(model_dir, 'command_history.pkl')
+        
+
+        f = open(command_history_file, 'w')
+        
+        pickle.dump(self.command_history, f)
+        f.close()
+    
+    def clear_history(self):
+        
+        this_path = os.path.abspath(__file__)
+        model_dir = os.path.dirname(this_path)
+        command_history_file = os.path.join(model_dir, 'command_history.pkl')
+        
+        if os.path.isfile(command_history_file):
+            os.remove(command_history_file)
+        
+        self.command_history = []
